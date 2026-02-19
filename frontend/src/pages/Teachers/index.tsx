da24d693ad141fc4865e41627beaf2434f3de00c
sasi72 <sasi.official07@gmail.com>
2026-02-19 23:17:07 +0530

    Teachers page variable initialitaion

--- frontend/src/pages/Teachers/index.tsx
+++ frontend/src/pages/Teachers/index.tsx
@@ -2,4 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { Plus, Search, Edit2, Trash2, Mail, Phone, Filter} from 'lucide-react';
 import api from '../../lib/axios';
 import toast from 'react-hot-toast';
-import { Teachers, Subjects, Class } from '../../types';
+import { Teacher, Subject, Class } from '../../types';
+
+const Teachers: React.FC = () => {
+    const [teachers, setTeachers] = useState<Teachers[]>([]);
+    const [subjects, setSubjects] = useState<Subject[]>([]);
+    const [classes, setClasses] = useState<Class[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const [selectedTeachers, setSelectedTeachers] = useState<Teachers | null>(null);
+    const [fromData, setFormData] = useState({
+        employeeId: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        addredd: '',
+        dateOfBirth: '',
+        gender: '',
+        qualification: '',
+        subject: [] as string[],
+        classes: [] as string[],
+        joiningDate: '',
+        salary: '',
+        imageUrl: '',
+    });
+
+}
+
+export default Teachers;
